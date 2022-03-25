f5b4f08fc51ea9bc4d600599c6e803fd4d859551
Vishwanath Hosdurga Suresh <vs542176@dal.ca>
2022-03-25 17:54:24 -0300

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -27,8 +27,6 @@ app.use("/api/careers", CareersRouter)
 
 app.use("/api/payments", PaymentRouter)
 
-app.use("/api/careers", CareersRouter)
-    
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
