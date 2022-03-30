2c182fc5be166d4c8a266a780c1c1d360c3fcfd4
Vishnu Sumanth Dampetla <vs683406@dal.ca>
2022-03-30 00:02:33 -0300

    solving conflicts

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,12 +1,10 @@
 // @Author: Kishan Thakkar
-const { Sequelize } = require('sequelize');
-require('dotenv').config({ path: require('find-config')('.env') })
-const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST } = process.env
-// console.log(DB_HOST)
+const { Sequelize } = require("sequelize");
+const { DB_DATABASE, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const DBConnection = new Sequelize(DB_DATABASE, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: 3306,
-  dialect: 'mysql',
+  dialect: "mysql",
 });
 
-module.exports = DBConnection
+module.exports = DBConnection;
